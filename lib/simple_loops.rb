bfb2802f797cef31dc45d98b1075f60436564966
Dominic DeJesus <domludejesus@gmail.com>
2020-08-01 22:26:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,8 +29,8 @@ end
 def return_string_array(array)
   count = 0 
   while count <array.length do 
-    
-    count += 1 
+    new_array.push(array[count].to_s) 
+     count += 1 
   end 
-  
+  new_array
 end 
